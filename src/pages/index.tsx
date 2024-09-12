999c8785817d15b5084786a76cbdf070551a258d
unknown <kemalcrisannnaufal@gmail.com>
2024-09-12 14:02:33 +0700

    add initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const InitialPage = () => {
   const { push } = useRouter();
   const { data } = useSession();
 
-  return <>{data?.user ? push("/dashboard") : push("/auth/login")}</>;
+  useEffect(() => {
+    if (data?.user) {
+      push("/dashboard");
+    } else {
+      push("/auth/login");
+    }
+  }, [data, push]);
+
+  return null; // Tidak perlu merender apapun karena hanya redirect
 };
 
 export default InitialPage;
